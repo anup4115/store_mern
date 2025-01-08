e31c438884799bfaa8e39a35af7da55d32b78482
Anup Prajapati <46585248+anup4115@users.noreply.github.com>
2025-01-08 10:59:38 +0545

    Add files via upload

---
+++ server/models/bookModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const bookSchema = new mongoose.Schema({
+	title: String,
+	author: String,
+	genre: String,
+	description: String,
+	price: Number,
+	image: String,
+});
+
+const Book = mongoose.model('Book', bookSchema);
+export default Book
