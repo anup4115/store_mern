75f988dcf7fe29a8eacffb6730bdd9517e0e0d94
Anup Prajapati <46585248+anup4115@users.noreply.github.com>
2025-01-23 08:58:12 +0545

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+// client/src/App.js
+
+
+import ProductList from './components/ProductList';
+import Header from './components/Header';
+import './App.css'
+import CustomItemContext from './context/ItemContext';
+
+const App = () => {
+	return (
+		<CustomItemContext>
+			<Header />
+			<ProductList />
+		</CustomItemContext>
+	);
+};
+
+export default App;
+
